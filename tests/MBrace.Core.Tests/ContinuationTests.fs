23ed08558b9ad8de448f541044825618e7515fb8
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-10-17 19:29:45 +0300

    reorganize test project

--- tests/MBrace.Core.Tests/ContinuationTests.fs
+++ tests/MBrace.Core.Tests/ContinuationTests.fs
@@ -1,4 +1,4 @@
-﻿namespace Nessos.MBrace.Core.Tests
+﻿namespace Nessos.MBrace.Tests
 
     open System
     open System.Threading
@@ -9,42 +9,6 @@
     open Nessos.MBrace
 
     #nowarn "444"
-
-    type Cloud =
-        static member RunProtected(comp, ?resources) =
-            try Cloud.RunSynchronously(comp, ?resources = resources, ?cancellationToken = None) |> Choice1Of2
-            with e -> Choice2Of2 e
-
-        static member RunProtected(comp : CancellationTokenSource -> Cloud<'T>, ?resources) =
-            let cts = new System.Threading.CancellationTokenSource()
-            let comp = comp cts
-            try Cloud.RunSynchronously(comp, ?resources = resources, cancellationToken = cts.Token) |> Choice1Of2
-            with e -> Choice2Of2 e
-
-    module Choice =
-
-        let shouldEqual (value : 'T) (input : Choice<'T, exn>) = 
-            match input with
-            | Choice1Of2 v' -> should equal value v'
-            | Choice2Of2 e -> should equal value e
-
-        let shouldMatch (pred : 'T -> bool) (input : Choice<'T, exn>) =
-            match input with
-            | Choice1Of2 t when pred t -> ()
-            | Choice1Of2 t -> raise <| new AssertionException(sprintf "value '%A' does not match predicate." t)
-            | Choice2Of2 e -> should be instanceOfType<'T> e
-
-        let shouldFailwith<'T, 'Exn when 'Exn :> exn> (input : Choice<'T, exn>) = 
-            match input with
-            | Choice1Of2 t -> should be instanceOfType<'Exn> t
-            | Choice2Of2 e -> should be instanceOfType<'Exn> e
-
-    type DummyDisposable() =
-        let isDisposed = ref false
-        interface ICloudDisposable with
-            member __.Dispose () = async { isDisposed := true }
-
-        member __.IsDisposed = !isDisposed
 
     [<TestFixture>]
     module ContinuationTests =
