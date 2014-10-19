76f03b7cb7855e216e52dc272d476fa844349397
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-10-19 22:32:31 +0300

    add Choice unit tests

--- tests/MBrace.Core.Tests/InMemoryTests.fs
+++ tests/MBrace.Core.Tests/InMemoryTests.fs
@@ -1,150 +1,288 @@
 ﻿namespace Nessos.MBrace.Tests
     
-    open System
-    open System.Threading
+open System
+open System.Threading
 
-    open NUnit.Framework
-    open FsUnit
+open NUnit.Framework
+open FsUnit
 
-    open Nessos.MBrace
-    open Nessos.MBrace.InMemory
+open Nessos.MBrace
+open Nessos.MBrace.InMemory
 
-    [<TestFixture>]
-    module InMemoryTests =
+[<TestFixture>]
+module InMemoryTests =
         
-        let run (workflow : Cloud<'T>) = Cloud.RunProtected(workflow, resources = InMemoryRuntime.Resource)
-        let runCts (workflow : CancellationTokenSource -> Cloud<'T>) = Cloud.RunProtected(workflow, resources = InMemoryRuntime.Resource)
+    let run (workflow : Cloud<'T>) = Cloud.RunProtected(workflow, resources = InMemoryRuntime.Resource)
+    let runCts (workflow : CancellationTokenSource -> Cloud<'T>) = Cloud.RunProtected(workflow, resources = InMemoryRuntime.Resource)
 
 
-        [<Test>]
-        let ``Parallel : empty input`` () =
-            run (Cloud.Parallel [||]) |> Choice.shouldEqual [||]
+    [<Test>]
+    let ``Parallel : empty input`` () =
+        run (Cloud.Parallel [||]) |> Choice.shouldEqual [||]
 
-        [<Test>]
-        let ``Parallel : simple inputs`` () =
-            cloud {
-                let f i = cloud { return i + 1 }
-                let! results = Array.init 100 f |> Cloud.Parallel
-                return Array.sum results
-            } |> run |> Choice.shouldEqual 5050
+    [<Test>]
+    let ``Parallel : simple inputs`` () =
+        cloud {
+            let f i = cloud { return i + 1 }
+            let! results = Array.init 100 f |> Cloud.Parallel
+            return Array.sum results
+        } |> run |> Choice.shouldEqual 5050
 
-        [<Test>]
-        let ``Parallel : simple nested`` () =
-            cloud {
-                let f i j = cloud { return i + j + 2 }
-                let cluster i = Array.init 10 (f i) |> Cloud.Parallel
-                let! results = Array.init 10 cluster |> Cloud.Parallel
-                return Array.concat results |> Array.sum
-            } |> run |> Choice.shouldEqual 1100
+    [<Test>]
+    let ``Parallel : simple nested`` () =
+        cloud {
+            let f i j = cloud { return i + j + 2 }
+            let cluster i = Array.init 10 (f i) |> Cloud.Parallel
+            let! results = Array.init 10 cluster |> Cloud.Parallel
+            return Array.concat results |> Array.sum
+        } |> run |> Choice.shouldEqual 1100
             
-        [<Test>]
-        let ``Parallel : simple exception`` () =
-            cloud {
-                let f i = cloud { return if i = 55 then invalidOp "failure" else i + 1 }
-                let! results = Array.init 100 f |> Cloud.Parallel
-                return Array.sum results
-            } |> run |> Choice.shouldFailwith<_, InvalidOperationException>
+    [<Test>]
+    let ``Parallel : simple exception`` () =
+        cloud {
+            let f i = cloud { return if i = 55 then invalidOp "failure" else i + 1 }
+            let! results = Array.init 100 f |> Cloud.Parallel
+            return Array.sum results
+        } |> run |> Choice.shouldFailwith<_, InvalidOperationException>
 
-        [<Test>]
-        [<Repeat(10)>]
-        let ``Parallel : exception contention`` () =
-            let counter = ref 0
-            cloud {
-                let f _ = cloud { return invalidOp "failure" }
-                try
-                    let! _ = Array.init 100 f |> Cloud.Parallel
-                    return raise <| new AssertionException("Cloud.Parallel should not have completed succesfully.")
-                with :? InvalidOperationException ->
-                    Interlocked.Increment counter |> ignore
-                    return ()
-            } |> run |> Choice.shouldEqual ()
-
-            // test that exception continuation was fired precisely once
-            !counter |> should equal 1
-
-        [<Test>]
-        [<Repeat(10)>]
-        let ``Parallel : exception cancellation`` () =
-            cloud {
-                let counter = ref 0
-                let worker i = cloud { 
-                    if i = 13 then
-                        do! Cloud.Sleep 100
-                        invalidOp "failure"
-                    else
-                        do! Cloud.Sleep 1000
-                        let _ = Interlocked.Increment counter
-                        return ()
-                }
-
-                try
-                    let! _ = Array.init 20 worker |> Cloud.Parallel
-                    return raise <| new AssertionException("Cloud.Parallel should not have completed succesfully.")
-                with :? InvalidOperationException ->
-                    return !counter
-            } |> run |> Choice.shouldEqual 0
-
-        [<Test>]
-        [<Repeat(10)>]
-        let ``Parallel : nested exception cancellation`` () =
-            cloud {
-                let counter = ref 0
-                let worker i j = cloud {
-                    if i = 6 && j = 5 then
-                        do! Cloud.Sleep 100
-                        invalidOp "failure"
-                    else
-                        do! Cloud.Sleep 1000
-                        let _ = Interlocked.Increment counter
-                        return ()
-                }
-
-                let cluster i = Array.init 10 (worker i) |> Cloud.Parallel |> Cloud.Ignore
-                try
-                    do! Array.init 10 cluster |> Cloud.Parallel |> Cloud.Ignore
-                    return raise <| new AssertionException("Cloud.Parallel should not have completed succesfully.")
-                with :? InvalidOperationException ->
-                    return !counter
-            } |> run |> Choice.shouldEqual 0
-            
-
-        [<Test>]
-        [<Repeat(10)>]
-        let ``Parallel : cancellation`` () =
-            let counter = ref 0
-            runCts(fun cts -> cloud {
-                let parallelTasks = cloud {
-                    let f i = cloud { 
-                        do! Cloud.Sleep 1000 
-                        return Interlocked.Increment counter }
-
-                    do! Array.init 10 f |> Cloud.Parallel |> Cloud.Ignore
-                }
-
-                let! _ = 
-                    parallelTasks <||> 
-                        cloud { 
-                            do! Cloud.Sleep 100 
-                            cts.Cancel () 
-                        }
-
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Parallel : exception contention`` () =
+        let counter = ref 0
+        cloud {
+            let f _ = cloud { return invalidOp "failure" }
+            try
+                let! _ = Array.init 100 f |> Cloud.Parallel
+                return raise <| new AssertionException("Cloud.Parallel should not have completed succesfully.")
+            with :? InvalidOperationException ->
+                Interlocked.Increment counter |> ignore
                 return ()
-            }) |> Choice.shouldFailwith<_, OperationCanceledException>
+        } |> run |> Choice.shouldEqual ()
 
-            !counter |> should equal 0
+        // test that exception continuation was fired precisely once
+        !counter |> should equal 1
 
-        [<Test>]
-        [<Repeat(10)>]
-        let ``Parallel : to sequential`` () =
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Parallel : exception cancellation`` () =
+        cloud {
+            let counter = ref 0
+            let worker i = cloud { 
+                if i = 13 then
+                    do! Cloud.Sleep 100
+                    invalidOp "failure"
+                else
+                    do! Cloud.Sleep 1000
+                    let _ = Interlocked.Increment counter
+                    return ()
+            }
+
+            try
+                let! _ = Array.init 20 worker |> Cloud.Parallel
+                return raise <| new AssertionException("Cloud.Parallel should not have completed succesfully.")
+            with :? InvalidOperationException ->
+                return !counter
+        } |> run |> Choice.shouldEqual 0
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Parallel : nested exception cancellation`` () =
+        cloud {
+            let counter = ref 0
+            let worker i j = cloud {
+                if i = 6 && j = 5 then
+                    do! Cloud.Sleep 100
+                    invalidOp "failure"
+                else
+                    do! Cloud.Sleep 1000
+                    let _ = Interlocked.Increment counter
+                    return ()
+            }
+
+            let cluster i = Array.init 10 (worker i) |> Cloud.Parallel |> Cloud.Ignore
+            try
+                do! Array.init 10 cluster |> Cloud.Parallel |> Cloud.Ignore
+                return raise <| new AssertionException("Cloud.Parallel should not have completed succesfully.")
+            with :? InvalidOperationException ->
+                return !counter
+        } |> run |> Choice.shouldEqual 0
+            
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Parallel : cancellation`` () =
+        let counter = ref 0
+        runCts(fun cts -> cloud {
+            let parallelTasks = cloud {
+                let f i = cloud { 
+                    do! Cloud.Sleep 1000 
+                    return Interlocked.Increment counter }
+
+                do! Array.init 10 f |> Cloud.Parallel |> Cloud.Ignore
+            }
+
+            let! _ = 
+                parallelTasks <||> 
+                    cloud { 
+                        do! Cloud.Sleep 100 
+                        cts.Cancel () 
+                    }
+
+            return ()
+        }) |> Choice.shouldFailwith<_, OperationCanceledException>
+
+        !counter |> should equal 0
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Parallel : to sequential`` () =
+        // check sequential semantics are forced by using
+        // deliberately not thread-safe code.
+        cloud {
+            let counter = ref 0
+            let seqWorker _ = cloud {
+                let init = !counter + 1
+                counter := init
+                do! Cloud.Sleep 10
+                return !counter = init
+            }
+
+            let! results = Array.init 20 seqWorker |> Cloud.Parallel |> Cloud.ToSequential
+            return Array.forall id results
+        } |> run |> Choice.shouldEqual true
+
+
+    [<Test>]
+    let ``Choice : empty input`` () =
+        Cloud.Choice [] |> run |> Choice.shouldEqual None
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Choice : all inputs 'None'`` () =
+        cloud {
+            let count = ref 0
+            let worker _ = cloud {
+                let _ = Interlocked.Increment count
+                return None
+            }
+
+            let! result = Array.init 100 worker |> Cloud.Choice
+            return (!count, result)
+        } |> run |> Choice.shouldEqual (100, None)
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Choice : one input 'Some'`` () =
+        cloud {
+            let count = ref 0
+            let worker i = cloud {
+                if i = 15 then return Some i
+                else
+                    do! Cloud.Sleep 100
+                    // check proper cancellation while we're at it.
+                    let _ = Interlocked.Increment count
+                    return None
+            }
+
+            let! result = Array.init 100 worker |> Cloud.Choice
+            return result, !count
+        } |> run |> Choice.shouldEqual (Some 15, 0)
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Choice : all inputs 'Some'`` () =
+        let successcounter = ref 0
+        cloud {
+            let worker _ = cloud { return Some 42 }
+            let! result = Array.init 100 worker |> Cloud.Choice
+            let _ = Interlocked.Increment successcounter
+            return result
+        } |> run |> Choice.shouldEqual (Some 42)
+
+        // ensure only one success continuation call
+        !successcounter |> should equal 1
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Choice : simple nested`` () =
+        cloud {
+            let counter = ref 0
+            let worker i j = cloud {
+                if i = 5 && j = 3 then
+                    do! Cloud.Sleep 100
+                    return Some(i,j)
+                else
+                    do! Cloud.Sleep 1000
+                    let _ = Interlocked.Increment counter
+                    return None
+            }
+
+            let cluster i = Array.init 10 (worker i) |> Cloud.Choice
+            let! result = Array.init 10 cluster |> Cloud.Choice
+            return !counter = 0, result
+        } |> run |> Choice.shouldEqual (true, Some (5,3))
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Choice : nested exception cancellation`` () =
+        let counter = ref 0
+        cloud {
+            let worker i j = cloud {
+                if i = 5 && j = 3 then
+                    do! Cloud.Sleep 100
+                    return invalidOp "failure"
+                else
+                    do! Cloud.Sleep 1000
+                    let _ = Interlocked.Increment counter
+                    return Some 42
+            }
+
+            let cluster i = Array.init 10 (worker i) |> Cloud.Choice
+            return! Array.init 10 cluster |> Cloud.Choice
+        } |> run |> Choice.shouldFailwith<_, InvalidOperationException>
+
+        !counter |> should equal 0
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Choice : simple cancellation`` () =
+        let taskCount = ref 0
+        runCts(fun cts ->
             cloud {
-                let counter = ref 0
-                let seqWorker _ = cloud {
-                    let init = !counter + 1
-                    counter := init
-                    do! Cloud.Sleep 10
-                    return !counter = init
+                let worker i = cloud {
+                    if i = 55 then
+                        do! Cloud.Sleep 10
+                        cts.Cancel()
+                        return Some 42
+                    else
+                        do! Cloud.Sleep 100
+                        let _ = Interlocked.Increment taskCount
+                        return Some 42
                 }
 
-                let! results = Array.init 20 seqWorker |> Cloud.Parallel |> Cloud.ToSequential
-                return Array.forall id results
-            } |> run |> Choice.shouldEqual true
+                return! Array.init 100 worker |> Cloud.Choice
+        }) |> Choice.shouldFailwith<_, OperationCanceledException>
+
+        !taskCount |> should equal 0
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Choice : to Sequential`` () =
+        // check sequential semantics are forced by using
+        // deliberately not thread-safe code.
+        cloud {
+            let counter = ref 0
+            let seqWorker i = cloud {
+                let init = !counter + 1
+                counter := init
+                do! Cloud.Sleep 10
+                !counter |> should equal init
+                if i = 16 then
+                    return Some ()
+                else
+                    return None
+            }
+
+            let! result = Array.init 20 seqWorker |> Cloud.Choice |> Cloud.ToSequential
+            return result, !counter
+        } |> run |> Choice.shouldEqual (Some(), 17)
