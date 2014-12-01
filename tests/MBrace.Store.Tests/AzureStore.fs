117676370f9463e099295d5088e7a757293145bf
krontogiannis <rodoconst@hotmail.com>
2014-12-01 16:43:53 +0200

    Azure blob store implementation

---
+++ tests/MBrace.Store.Tests/AzureStore.fs
@@ -0,0 +1,30 @@
+﻿namespace Nessos.MBrace.Store.Tests.Azure
+
+open NUnit.Framework
+open FsUnit
+
+open Nessos.MBrace
+open Nessos.MBrace.Store
+open Nessos.MBrace.Azure.Store
+open Nessos.MBrace.Store.Tests
+
+
+module Helper =
+    open System
+    let conn = Environment.GetEnvironmentVariable("azurestorageconn", EnvironmentVariableTarget.User)
+    let store = lazy new BlobStore(conn)
+
+[<TestFixture>]
+type ``Azure Blob store tests`` () =
+    inherit  ``File Store Tests``(Helper.store.Value)
+
+    static do
+        StoreRegistry.Register(Helper.store.Value)
+
+//[<TestFixture>]
+//type ``FileSystem Table store tests`` () =
+//    inherit  ``Table Store Tests``(StoreConfiguration.fileSystemStore)
+//
+//[<TestFixture>]
+//type ``FileSystem MBrace tests`` () =
+//    inherit ``Local MBrace store tests``(StoreConfiguration.fileSystemStore, StoreConfiguration.fileSystemStore)
