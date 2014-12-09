898b7099f22351074c9bf45321f1194af5cfd95a
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-12-09 18:35:16 +0200

    FileStoreCache

--- src/MBrace.Runtime.Core/Store/FileStoreCache.fs
+++ src/MBrace.Runtime.Core/Store/FileStoreCache.fs
@@ -20,14 +20,43 @@ type CacheBehavior =
     /// Cache on read/write.
     | Default = 3uy
 
+// Combines two write-only streams into one.
+type private StreamCombiner(s1 : Stream, s2 : Stream) =
+    inherit Stream()
+
+    override __.Length = s1.Length
+    override __.ReadTimeout = min s1.ReadTimeout s2.ReadTimeout
+    override __.Position 
+        with get () = s1.Position
+        and set p = s1.Position <- p ; s2.Position <- p
+
+    override __.CanRead = false
+    override __.Read(_,_,_) = raise <| new NotSupportedException()
+
+    override __.CanSeek = s1.CanSeek && s2.CanSeek
+    override __.Seek(i,o) = 
+        let i1 = s1.Seek(i,o) 
+        let i2 = s2.Seek(i,o)
+        i1
+
+    override __.CanTimeout = s1.CanTimeout || s2.CanTimeout
+    override __.WriteTimeout = min s1.WriteTimeout s2.WriteTimeout
+    override __.CanWrite = true
+    override __.WriteByte(b : byte) = s1.WriteByte b ; s2.WriteByte b
+    override __.Write(buf : byte[], offset, count) = s1.Write(buf, offset, count) ; s2.Write(buf, offset, count)
+
+    override __.SetLength l = s1.SetLength l ; s2.SetLength l
+    override __.Flush () = s1.Flush() ; s2.Flush()
+
+
 [<AutoSerializable(false) ; Sealed>]
 type FileStoreCache private (cacheStore : ICloudFileStore, sourceStore : ICloudFileStore, 
-                                    cacheContainer : string, ?cacheBehavior : CacheBehavior) =
-
+                                cacheContainer : string, cacheBehavior : CacheBehavior, 
+                                descr : ICloudFileStore -> ICloudFileStoreDescriptor) =
 
     let getCachedFileName (path : string) = cacheStore.Combine [|cacheContainer ; Convert.StringToBase32 path|]
 
-    let mutable cacheBehavior = defaultArg cacheBehavior CacheBehavior.Default
+    let mutable cacheBehavior = cacheBehavior
 
     member this.Container = cacheContainer
 
@@ -41,93 +70,81 @@ type FileStoreCache private (cacheStore : ICloudFileStore, sourceStore : ICloudF
             if cacheBehavior.HasFlag CacheBehavior.OnRead then
                 let cachedFileName = getCachedFileName path
 
-                let rec attemptRead () = async {
+                let rec attemptRead retries = async {
                     let! sourceExists, cacheExists = Async.Parallel(sourceStore.FileExists path, cacheStore.FileExists cachedFileName)
 
                     if sourceExists then
                         if cacheExists then
                             try return! cacheStore.BeginRead cachedFileName
-                            with e ->
+                            with e when retries > 0 ->
                                 // retry in case of concurrent cache writes
-                                return! retryAsync (RetryPolicy.Retry(3, 0.5<sec>)) (attemptRead())
+                                return! attemptRead (retries - 1)
                         else
                             try
                                 use! stream = sourceStore.BeginRead path
                                 in do! cacheStore.OfStream(stream, cachedFileName)
 
                                 return! cacheStore.BeginRead cachedFileName
-                            with e ->
+                            with e when retries > 0 ->
                                 // retry in case of concurrent cache writes
-                                return! retryAsync (RetryPolicy.Retry(3, 0.5<sec>)) (attemptRead())
-//                    elif cacheExists then
-//                        // this is the old behaviour; doesn't feel right
-//                        return invalidOp <| sprintf "Incoherent cache : '%s' found in cache but not in the main store" path
+                                return! attemptRead (retries - 1)
                     else
                         return raise <| FileNotFoundException(path)
                 }
 
-                return! attemptRead()
+                return! attemptRead 3
             else
                 return! sourceStore.BeginRead path
         }
         
-        member x.BeginWrite(path: string): Async<Stream> =
-            failwith "Not implemented yet"
+        member x.BeginWrite(path: string): Async<Stream> = async {
+            if cacheBehavior.HasFlag CacheBehavior.OnWrite then
+                let cachedFile = getCachedFileName path
+                // check if file exists in local cache first.
+                let! cachedFileExists = Async.StartChild(cacheStore.FileExists cachedFile)
+                let! sourceStream = sourceStore.BeginWrite path
+                let! cachedFileExists = cachedFileExists
+                if cachedFileExists then do! retryAsync (RetryPolicy.Retry(3, 0.5<sec>)) (cacheStore.DeleteFile cachedFile)
+                let! cacheStream = cacheStore.BeginWrite(getCachedFileName path)
+                return new StreamCombiner(sourceStream, cacheStream) :> Stream
+            else
+                return! sourceStore.BeginWrite path
+        }
         
-        member x.Combine(paths: string []): string = 
-            failwith "Not implemented yet"
+        member x.Combine(paths: string []): string = sourceStore.Combine paths
         
-        member x.CreateDirectory(directory: string): Async<unit> = 
-            failwith "Not implemented yet"
+        member x.CreateDirectory(directory: string): Async<unit> = sourceStore.CreateDirectory directory
         
-        member x.CreateUniqueDirectoryPath(): string = 
-            failwith "Not implemented yet"
+        member x.CreateUniqueDirectoryPath(): string = sourceStore.CreateUniqueDirectoryPath()
         
-        member x.DeleteDirectory(directory: string, recursiveDelete: bool): Async<unit> = 
-            failwith "Not implemented yet"
+        member x.DeleteDirectory(directory: string, recursiveDelete: bool): Async<unit> = sourceStore.DeleteDirectory(directory, recursiveDelete)
         
-        member x.DeleteFile(path: string): Async<unit> = 
-            failwith "Not implemented yet"
+        member x.DeleteFile(path: string): Async<unit> = sourceStore.DeleteFile(path)
         
-        member x.DirectoryExists(directory: string): Async<bool> = 
-            failwith "Not implemented yet"
+        member x.DirectoryExists(directory: string): Async<bool> = sourceStore.DirectoryExists(directory)
         
-        member x.EnumerateDirectories(directory: string): Async<string []> = 
-            failwith "Not implemented yet"
+        member x.EnumerateDirectories(directory: string): Async<string []> = sourceStore.EnumerateDirectories(directory)
         
-        member x.EnumerateFiles(directory: string): Async<string []> = 
-            failwith "Not implemented yet"
+        member x.EnumerateFiles(directory: string): Async<string []> = sourceStore.EnumerateFiles(directory)
         
-        member x.FileExists(path: string): Async<bool> = 
-            failwith "Not implemented yet"
+        member x.FileExists(path: string): Async<bool> = sourceStore.FileExists(path)
         
-        member x.GetDirectoryName(path: string): string = 
-            failwith "Not implemented yet"
+        member x.GetDirectoryName(path: string): string = sourceStore.GetDirectoryName(path)
         
-        member x.GetFileName(path: string): string = 
-            failwith "Not implemented yet"
+        member x.GetFileName(path: string): string = sourceStore.GetFileName(path)
         
-        member x.GetFileSize(path: string): Async<int64> = 
-            failwith "Not implemented yet"
+        member x.GetFileSize(path: string): Async<int64> = sourceStore.GetFileSize(path)
         
-        member x.GetFileStoreDescriptor(): ICloudFileStoreDescriptor = 
-            failwith "Not implemented yet"
+        member x.GetFileStoreDescriptor(): ICloudFileStoreDescriptor = descr sourceStore
         
-        member x.GetRootDirectory(): string = 
-            failwith "Not implemented yet"
+        member x.GetRootDirectory(): string = sourceStore.GetRootDirectory()
         
-        member x.Id: string = 
-            failwith "Not implemented yet"
+        member x.Id: string = sourceStore.Id
         
-        member x.Name: string = 
-            failwith "Not implemented yet"
+        member x.Name: string = sourceStore.Name
         
-        member x.OfStream(source: Stream, target: string): Async<unit> = 
-            failwith "Not implemented yet"
+        member x.OfStream(source: Stream, target: string): Async<unit> = sourceStore.OfStream(source, target)
         
-        member x.ToStream(sourceFile: string, target: Stream): Async<unit> = 
-            failwith "Not implemented yet"
+        member x.ToStream(sourceFile: string, target: Stream): Async<unit> = sourceStore.ToStream(sourceFile, target)
         
-        member x.TryGetFullPath(path: string): string option = 
-            failwith "Not implemented yet"
-        
+        member x.TryGetFullPath(path: string): string option = sourceStore.TryGetFullPath(path)
