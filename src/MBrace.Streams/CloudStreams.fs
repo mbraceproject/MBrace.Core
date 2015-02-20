5895c4cf835515b9db9f0b83099342c1508bf447
npal <nick.palladinos@gmail.com>
2015-02-20 15:55:29 +0200

    foldBy perf fix

--- src/MBrace.Streams/CloudStreams.fs
+++ src/MBrace.Streams/CloudStreams.fs
@@ -398,7 +398,7 @@ module CloudStream =
         // Phase 1
         let shuffling = 
             cloud {
-                let combiner' (left : CloudVector<_>) (right : CloudVector<_>) =  CloudVector.Merge [| left ; right |]
+                let combiner' (left : _ []) (right : _ []) =  Array.append left right 
                 let! totalWorkers = match stream.DegreeOfParallelism with Some n -> cloud { return n } | None -> getWorkerCount()
                 let! keyValueArray = stream.Apply (collectorf totalWorkers) 
                                                   (fun keyValues -> cloud {
@@ -407,11 +407,10 @@ module CloudStream =
                                                             let! values = CloudVector.New(value, maxCloudVectorPartitionSize, enableCaching = false)
                                                             dict.[key] <- values
                                                         let values = dict |> Seq.map (fun keyValue -> (keyValue.Key, keyValue.Value)) 
-                                                        return! CloudVector.New(values, maxCloudVectorPartitionSize, enableCaching = false) }) combiner'
+                                                        return Seq.toArray values }) combiner'
                 
-                let! kva = keyValueArray.ToEnumerable()
                 let merged =
-                    kva
+                    keyValueArray
                     |> Seq.groupBy fst
                     |> Seq.map (fun (i,kva) -> i, kva |> Seq.map snd |> CloudVector.Merge)
                     |> Seq.toArray
