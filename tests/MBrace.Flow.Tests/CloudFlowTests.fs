1edfc8282a44b5c8ddef1156e56539a48a1b684e
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-04-11 21:05:19 +0300

    fix take unit test

--- tests/MBrace.Flow.Tests/CloudFlowTests.fs
+++ tests/MBrace.Flow.Tests/CloudFlowTests.fs
@@ -374,9 +374,6 @@ type ``CloudFlow tests`` () as self =
         let f (xs : int[], n : int) =
             let n = System.Math.Abs(n)
             let x = xs |> CloudFlow.ofArray |> CloudFlow.take n |> CloudFlow.toArray |> run
-            // is this assertion correct?
-//            let y = xs.Take(n).ToArray()
-//            Assert.AreEqual(y, x)
             Assert.AreEqual(min xs.Length n, x.Length)
         Check.QuickThrowOnFail(f, self.FsCheckMaxNumberOfTests)
 
