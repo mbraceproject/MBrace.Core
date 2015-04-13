912ee545f49a540e587860eec2cd798fd35c0a99
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-04-13 18:11:11 +0300

    improve dict tests

--- tests/MBrace.Core.Tests/StoreTests/CloudDictionaryTests.fs
+++ tests/MBrace.Core.Tests/StoreTests/CloudDictionaryTests.fs
@@ -74,11 +74,11 @@ type ``CloudDictionary Tests`` (parallelismFactor : int) as self =
         let parallelismFactor = parallelismFactor
         cloud {
             let! dict = CloudDictionary.New<int> ()
-            let update i = local {
-                let! _ = dict.AddOrUpdate("key", function None -> 1 | Some i -> i + 1)
+            let incr i = local {
+                let! _ = dict.AddOrUpdate("key", function None -> i | Some c -> c + i)
                 return ()
             }
 
-            do! Cloud.Parallel [ for i in 1 .. 100 -> update i] |> Cloud.Ignore
+            do! Cloud.Parallel [ for i in 1 .. 100 -> incr i ] |> Cloud.Ignore
             return! dict.TryFind "key"
-        } |> runRemote |> shouldEqual (Some 100)
+        } |> runRemote |> shouldEqual (Some 5050)
