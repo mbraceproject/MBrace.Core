24badd4a5172ddbcca80180a8da54c6ecd7d1c13
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-04-14 18:13:49 +0300

    update vagabond

--- build.fsx
+++ build.fsx
@@ -261,7 +261,7 @@ Target "NuGet.Runtime.Core" (fun _ ->
                 [
                     ("MBrace.Core", RequireExactly release.NugetVersion)
                     ("FsPickler", "1.0.16")
-                    ("Vagabond", "0.6.0")
+                    ("Vagabond", "0.6.2")
                     ("Unquote", "2.2.2")
                 ]
             Publish = hasBuildParam "nugetkey" 
