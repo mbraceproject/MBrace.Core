196e3aa3bbad92e27547f5c35856c3b117ddddbe
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-06-03 02:00:27 +0300

    migrate nuget authoring to paket

--- src/MBrace.Runtime.Core/Store/StoreEntity.fs
+++ src/MBrace.Runtime.Core/Store/StoreEntity.fs
@@ -20,11 +20,13 @@ type StorageEntity =
         let gathered = new Dictionary<string, ICloudStorageEntity> ()
         let visitor = 
             { new IObjectVisitor with
-                member __.Visit(element : 'a) =
+                member __.Visit(_,element : 'a) =
                     match box element with
                     | :? ICloudStorageEntity as e when not <| gathered.ContainsKey e.Id ->
                         gathered.Add(e.Id, e)
                     | _ -> ()
+
+                    true
             }
 
         FsPickler.VisitObject(visitor, graph)
