da9c3d005ae2fb3a64cdb625fb07f728f9c1bab5
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-06-05 21:25:37 +0300

    small refactoring

---
+++ src/MBrace.Runtime.Core/Runtime/Primitives.fs
@@ -0,0 +1,91 @@
+﻿namespace MBrace.Runtime
+
+open System
+
+open Nessos.FsPickler
+open Nessos.Vagabond
+
+open MBrace.Core
+open MBrace.Core.Internals
+open MBrace.Store
+
+open MBrace.Runtime.Utils
+
+/// Defines a serializable cancellation entry with global visibility that can
+/// be cancelled or polled for cancellation.
+type ICancellationEntry =
+    inherit IAsyncDisposable
+    /// Unique identifier for cancellation entry instance.
+    abstract UUID : string
+    /// Asynchronously checks if entry has been cancelled.
+    abstract IsCancellationRequested : Async<bool>
+    /// Asynchronously cancels the entry.
+    abstract Cancel : unit -> Async<unit>
+
+/// Defines a serializable cancellation entry manager with global visibility.
+type ICancellationEntryFactory =
+    /// Asynchronously creates a cancellation entry with no parents.
+    abstract CreateCancellationEntry : unit -> Async<ICancellationEntry>
+
+    /// <summary>
+    ///     Asynchronously creates a cancellation entry provided parents. 
+    ///     Returns 'None' if all parents have been cancelled.
+    /// </summary>
+    /// <param name="parents">Cancellation entry parents.</param>
+    abstract TryCreateLinkedCancellationEntry : parents:ICancellationEntry[] -> Async<ICancellationEntry option>
+
+/// Defines a distributed, atomic counter.
+type ICloudCounter =
+    inherit IAsyncDisposable
+    /// Asynchronously increments the counter by 1.
+    abstract Increment : unit -> Async<int>
+    /// Asynchronously decrements the counter by 1.
+    abstract Decrement : unit -> Async<int>
+    /// Asynchronously fetches the current value for the counter.
+    abstract Value     : Async<int>
+
+/// Defines a distributed result aggregator.
+type IResultAggregator<'T> =
+    inherit IAsyncDisposable
+    /// Declared capacity for result aggregator.
+    abstract Capacity : int
+    /// Asynchronously returns current accumulated size for aggregator.
+    abstract CurrentSize : Async<int>
+    /// Asynchronously returns if result aggregator has been completed.
+    abstract IsCompleted : Async<bool>
+    /// <summary>
+    ///     Asynchronously sets result at given index to aggregator.
+    ///     Returns true if aggregation is completed.
+    /// </summary>
+    /// <param name="index">Index to set value at.</param>
+    /// <param name="value">Value to be set.</param>
+    /// <param name="overwrite">Overwrite if value already exists at index.</param>
+    abstract SetResult : index:int * value:'T * overwrite:bool -> Async<bool>
+    /// <summary>
+    ///     Asynchronously returns aggregated results from aggregator.
+    /// </summary>
+    abstract ToArray : unit -> Async<'T []>
+
+
+/// Defines a distributed task completion source.
+type ICloudTaskCompletionSource =
+    inherit IAsyncDisposable
+    /// <summary>
+    ///     Asynchronously sets the the result to be an exception.
+    /// </summary>
+    /// <param name="edi">Exception dispatch info to be submitted.</param>
+    abstract SetException : edi:ExceptionDispatchInfo -> Async<unit>
+
+    /// <summary>
+    ///     Asynchronously declares the the result to be cancelled.
+    /// </summary>
+    /// <param name="edi">Exception dispatch info to be submitted.</param>
+    abstract SetCancelled : exn:OperationCanceledException -> Async<unit>
+
+/// Defines a distributed task completion source.
+type ICloudTaskCompletionSource<'T> =
+    inherit ICloudTaskCompletionSource
+    /// Gets the underlying cloud task for the task completion source.
+    abstract Task : ICloudTask<'T>
+    /// Asynchronously sets a completed result for the task.
+    abstract SetCompleted : 'T -> Async<unit>
---
+++ src/MBrace.Runtime.Core/Runtime/ResourceManager.fs
@@ -0,0 +1,77 @@
+﻿namespace MBrace.Runtime
+
+open System
+
+open Nessos.Vagabond
+
+open MBrace.Core
+open MBrace.Core.Internals
+
+/// Defines a Vagabond assembly manager
+type IAssemblyManager =
+    /// <summary>
+    ///     Uploads assemblies and data dependencies to runtime.
+    /// </summary>
+    /// <param name="assemblies">Local Vagabond assembly descriptors to be uploaded.</param>
+    abstract UploadAssemblies : assemblies:seq<VagabondAssembly> -> Async<unit>
+
+    /// <summary>
+    ///     Downloads assemblies and data dependencies from runtime.
+    /// </summary>
+    /// <param name="ids">Assembly identifiers to be downloaded.</param>
+    abstract DownloadAssemblies : ids:seq<AssemblyId> -> Async<VagabondAssembly []>
+
+    /// <summary>
+    ///     Loads provided vagabond assemblies to current application domain.
+    /// </summary>
+    /// <param name="assemblies">Assemblies to be loaded.</param>
+    abstract LoadAssemblies : assemblies:seq<VagabondAssembly> -> AssemblyLoadInfo []
+
+    /// <summary>
+    ///     Computes vagabond dependencies for provided object graph.
+    /// </summary>
+    /// <param name="graph">Object graph to be computed.</param>
+    abstract ComputeDependencies : graph:obj -> VagabondAssembly []
+
+    /// <summary>
+    ///     Registers a native dependency for instance.
+    /// </summary>
+    /// <param name="path"></param>
+    abstract RegisterNativeDependency : path:string -> VagabondAssembly
+
+    /// Gets native dependencies for assembly
+    abstract NativeDependencies : VagabondAssembly []
+
+/// Defines an abstract runtime resource provider. Should not be serializable
+type IRuntimeResourceManager =
+    /// Local MBrace resources to be supplied to workflow.
+    abstract ResourceRegistry : ResourceRegistry
+    /// Asynchronously returns all workers in the current cluster.
+    abstract GetAvailableWorkers : unit -> Async<IWorkerRef []>
+    /// Gets the WorkerRef identifying the current worker instance.
+    abstract CurrentWorker : IWorkerRef
+    /// Logger abstraction for user-specified cloud logging.
+    abstract GetCloudLogger : CloudJob -> ICloudLogger
+    /// Logger abstraction used for local worker logging.
+    abstract SystemLogger : ISystemLogger
+    /// Serializable distributed cancellation entry manager.
+    abstract CancellationEntryFactory : ICancellationEntryFactory
+    /// Gets the job queue instance for the runtime.
+    abstract JobQueue : IJobQueue
+    /// Gets the Vagabond assembly manager for this runtime.
+    abstract AssemblyManager : IAssemblyManager
+
+    /// <summary>
+    ///     Asynchronously creates a new distributed counter with supplied initial value.
+    /// </summary>
+    /// <param name="initialValue">Supplied initial value.</param>
+    abstract RequestCounter : initialValue:int -> Async<ICloudCounter>
+
+    /// <summary>
+    ///     Asynchronously creates a new result aggregator with supplied capacity.
+    /// </summary>
+    /// <param name="capacity">Number of items to be aggregated.</param>
+    abstract RequestResultAggregator<'T> : capacity:int -> Async<IResultAggregator<'T>>
+
+    /// Asynchronously creates a new distributed task completion source.
+    abstract RequestTaskCompletionSource<'T> : unit -> Async<ICloudTaskCompletionSource<'T>>
