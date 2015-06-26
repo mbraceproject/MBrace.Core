b6cbecdf80820eb150148d4d925da5734c13b7a4
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-06-26 17:21:12 +0300

    add comments

--- samples/MBrace.Thespian/Config.fs
+++ samples/MBrace.Thespian/Config.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Net
 open System.Reflection
 open System.Threading
 
@@ -26,6 +27,7 @@ type Config private () =
     static let isInitialized = ref false
     static let mutable objectCache = Unchecked.defaultof<InMemoryCache>
     static let mutable workingDirectory = Unchecked.defaultof<string>
+    static let mutable localEndpoint = Unchecked.defaultof<IPEndPoint>
 
     static let initVagabond populateDirs (path:string) =
         if populateDirs then ignore <| Directory.CreateDirectory path
@@ -37,9 +39,9 @@ type Config private () =
             invalidOp "Runtime configuration has not been initialized."
 
     /// <summary>
-    ///     Initializes the global configuration object
+    ///     Initializes the global configuration object.
     /// </summary>
-    /// <param name="populateDirs"></param>
+    /// <param name="populateDirs">Create or clear working directory.</param>
     static member Initialize(populateDirs : bool) =
         lock isInitialized (fun () ->
             if isInitialized.Value then invalidOp "Runtime configuration has already been initialized."
@@ -54,6 +56,7 @@ type Config private () =
             Nessos.Thespian.Serialization.defaultSerializer <- new FsPicklerMessageSerializer(VagabondRegistry.Instance.Serializer)
             Nessos.Thespian.Default.ReplyReceiveTimeout <- Timeout.Infinite
             TcpListenerPool.RegisterListener(IPEndPoint.any)
+            localEndpoint <- TcpListenerPool.GetListener().LocalEndPoint
             isInitialized := true)
 
     /// FsPickler serializer instance used by MBrace.Thespian
@@ -63,6 +66,6 @@ type Config private () =
     /// Object cache used by the instance
     static member ObjectCache = checkInitialized() ; objectCache :> IObjectCache
     /// TCP Endpoing used by the local Thespian instance
-    static member LocalEndPoint = checkInitialized() ; TcpListenerPool.GetListener().LocalEndPoint
+    static member LocalEndPoint = checkInitialized() ; localEndpoint
     /// Local TCP address used by the local Thespian instance
-    static member LocalAddress = checkInitialized() ; sprintf "%s:%d" TcpListenerPool.DefaultHostname (TcpListenerPool.GetListener().LocalEndPoint.Port)
+    static member LocalAddress = checkInitialized() ; sprintf "%s:%d" TcpListenerPool.DefaultHostname localEndpoint.Port
