9e304b59a2a7b25116dccbb39d6be96b2b8601e3
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-07-26 15:49:44 +0300

    clean up thespian tests.fsx

--- samples/MBrace.Thespian/test.fsx
+++ samples/MBrace.Thespian/test.fsx
@@ -14,54 +14,21 @@ open MBrace.Flow
 
 MBraceThespian.WorkerExecutable <- __SOURCE_DIRECTORY__ + "/../../bin/MBrace.Thespian.exe"
 
-let runtime = MBraceThespian.InitLocal 4
-runtime.AttachLogger(new ConsoleLogger())
+#time "on"
 
-let w = runtime.Workers
+let cluster = MBraceThespian.InitLocal 4
+cluster.AttachLogger(new ConsoleLogger())
 
-runtime.ShowProcessInfo()
-runtime.ShowWorkerInfo()
+let w = cluster.Workers
+
+cloud { return 42 } |> cluster.Run
+
+cluster.ShowProcessInfo()
+cluster.ShowWorkerInfo()
 
 let proc = 
     CloudFlow.OfHttpFileByLine "http://www.textfiles.com/etext/AUTHORS/SHAKESPEARE/shakespeare-alls-11.txt"
     |> CloudFlow.length
-    |> runtime.CreateProcess
+    |> cluster.CreateProcess
 
 proc.AwaitResult() |> Async.RunSynchronously
-
-#time "on"
-
-// vagabond data initialization test 1.
-let c = ref 0
-for i in 1 .. 10 do
-    c := runtime.Run(cloud { return !c + 1 })
-
-// vagabond data initialization test 2.
-let mutable enabled = false
-
-runtime.GetAllProcesses()
-
-runtime.ClearAllProcesses()
-
-runtime.GetWorkerInfo()
-
-runtime.ShowProcessInfo()
-
-runtime.Run(cloud { return enabled }, taskName = "test")
-
-enabled <- true
-
-runtime.Run(cloud { return enabled })
-
-enabled <- false
-
-runtime.Run(cloud { return enabled })
-
-cloud {
-    let client = new System.Net.WebClient()
-    return client
-} |> runtime.Run
-
-let cv = runtime.Run (CloudValue.New(32, enum 0))
-
-cv.Value
