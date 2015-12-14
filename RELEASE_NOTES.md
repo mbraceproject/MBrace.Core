b4ecb03d0554ea3fefef8c3978d6e068f72ec86d
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-12-15 00:22:15 +0200

    bump version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 1.0.7
+* Update FsPickler to latest version.
+
 #### 1.0.6
 * Fix packaging issue.
 
--- src/MBrace.CSharp/Properties/AssemblyInfo.cs
+++ src/MBrace.CSharp/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Nessos Information Technologies")]
 [assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")]
 [assembly: AssemblyTrademarkAttribute("MBrace")]
-[assembly: AssemblyVersionAttribute("1.0.6")]
-[assembly: AssemblyFileVersionAttribute("1.0.6")]
+[assembly: AssemblyVersionAttribute("1.0.7")]
+[assembly: AssemblyFileVersionAttribute("1.0.7")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0.6";
+        internal const string Version = "1.0.7";
     }
 }
