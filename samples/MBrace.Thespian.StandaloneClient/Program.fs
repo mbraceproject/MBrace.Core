1e51f17f2c79d31e0dd228c5047769fd0ad2dc37
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-01-26 13:39:43 +0200

    add MBrace.Thespian.StandaloneClient sample project

---
+++ samples/MBrace.Thespian.StandaloneClient/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open MBrace.Core
+open MBrace.Thespian
+
+[<EntryPoint>]
+let main argv = 
+
+    ThespianWorker.LocalExecutable <- __SOURCE_DIRECTORY__ + "/../../bin/mbrace.thespian.worker.exe"
+
+    let cluster = ThespianCluster.InitOnCurrentMachine(workerCount = 2, logger = new ConsoleLogger())
+
+    let result = cloud { return "Hello world!" } |> cluster.Run
+
+    printfn "Got result %A" result
+
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
