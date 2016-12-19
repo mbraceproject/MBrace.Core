46a71f41ba7e316dd1ebf0b32b267308324d2efe
Avi Avni <avi.avni@gmail.com>
2016-12-19 17:42:51 +0200

    basic graph proccessing

---
+++ src/MBrace.Graph/Graph.fs
@@ -0,0 +1,62 @@
+﻿namespace MBrace.Graph
+
+open MBrace.Core
+open MBrace.Flow
+
+type VertexId = int64
+
+type Node<'a> = 
+    { Id : VertexId
+      Attr : 'a }
+
+type Edge<'a> = 
+    { SrcId : VertexId
+      DstId : VertexId
+      Attr : 'a }
+
+type EdgeContext<'a, 'b, 'c> = 
+    { SrcId : VertexId
+      SrcAttr : Cloud<'a>
+      DstId : VertexId
+      DstAttr : Cloud<'a>
+      Attr : 'b
+      SendToSrc : 'c -> unit
+      SendToDst : 'c -> unit }
+
+type Graph<'a, 'b> = 
+    { Vertices : CloudFlow<Node<'a>>
+      Edges : CloudFlow<Edge<'b>> }
+
+module CloudGraph = 
+    let inline AggregateMessage<'a, 'b, 'm> (g : Graph<'a, 'b>) (sendMsg : EdgeContext<'a, 'b, 'm> -> unit) 
+               (accMsg : 'm -> 'm -> 'm) = 
+        let getAttr id = 
+            g.Vertices |> CloudFlow.pick (fun a -> 
+                              if a.Id = id then Some(a.Attr)
+                              else None)
+        
+        let sentTo (dict:CloudDictionary<'m>) (id:VertexId) accMsg m = 
+            dict.AddOrUpdate(id.ToString(), 
+                             (fun acc -> 
+                             match acc with
+                             | Some acc -> accMsg acc m
+                             | _ -> m))
+            |> ignore
+        
+        cloud { 
+            use! dict = CloudDictionary.New<'m>()
+            let! a = g.Edges
+                     |> CloudFlow.map (fun edge -> 
+                            let ctx = 
+                                { SrcId = edge.SrcId
+                                  SrcAttr = getAttr edge.SrcId
+                                  DstId = edge.DstId
+                                  DstAttr = getAttr edge.DstId
+                                  Attr = edge.Attr
+                                  SendToSrc = sentTo dict edge.SrcId accMsg
+                                  SendToDst = sentTo dict edge.DstId accMsg }
+                            sendMsg ctx)
+                     |> CloudFlow.toArray
+            a |> ignore
+            return dict
+        }
---
+++ src/MBrace.Graph/test.fsx
@@ -0,0 +1,58 @@
+﻿#I "../../bin/"
+#r "MBrace.Core.dll"
+#r "MBrace.Runtime.dll"
+#r "MBrace.Thespian.dll"
+#r "MBrace.Flow.dll"
+#r "MBrace.Graph.dll"
+#r "Streams.dll"
+
+open System
+open MBrace.Core
+open MBrace.Library
+open MBrace.Thespian
+open MBrace.Flow
+open MBrace.Flow.Fluent
+open MBrace.ThreadPool
+open MBrace.Graph
+
+ThespianWorker.LocalExecutable <- __SOURCE_DIRECTORY__ + "/../../bin/mbrace.thespian.worker.exe"
+
+let cluster = 
+    ThespianCluster.InitOnCurrentMachine(workerCount = 4, logLevel = LogLevel.Debug, logger = new ConsoleLogger())
+
+let nodes : Node<string> [] = 
+    [| { Id = 1L
+         Attr = "A" }
+       { Id = 2L
+         Attr = "B" }
+       { Id = 3L
+         Attr = "C" }
+       { Id = 4L
+         Attr = "D" }
+       { Id = 5L
+         Attr = "E" } |]
+
+let edges : Edge<int> [] = 
+    [| { SrcId = 1L
+         DstId = 2L
+         Attr = 10 }
+       { SrcId = 1L
+         DstId = 3L
+         Attr = 20 }
+       { SrcId = 1L
+         DstId = 4L
+         Attr = 30 }
+       { SrcId = 1L
+         DstId = 5L
+         Attr = 40 }
+       { SrcId = 1L
+         DstId = 1L
+         Attr = 50 } |]
+
+let g = 
+    { Vertices = nodes |> CloudFlow.OfArray
+      Edges = edges |> CloudFlow.OfArray }
+
+let res = CloudGraph.AggregateMessage<string, int, int> g (fun c -> c.SendToSrc c.Attr) (fun acc m -> acc + m)
+let a = res |> cluster.Run
+a.TryFind "1"
