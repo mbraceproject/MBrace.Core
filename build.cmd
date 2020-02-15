f28106b9c05dc7bd0f56c0c4a0fadaed6012a363
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-02-15 14:09:35 +0000

    use dotnet paket

--- build.cmd
+++ build.cmd
@@ -1,13 +1,6 @@
 @echo off
 
-.paket\paket.bootstrapper.exe
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-.paket\paket.exe restore -v
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
+dotnet tool restore
+dotnet paket restore
 
 packages\build\FAKE\tools\FAKE.exe build.fsx %*
--- build.sh
+++ build.sh
@@ -1,33 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+dotnet tool restore
+dotnet paket restore
+
 if [ "X$OS" = "XWindows_NT" ] ; then
-  # use .Net
-
-  .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  .paket/paket.exe restore -v
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
   packages/build/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx 
 else
-
-  # use mono
-  mono .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  mono .paket/paket.exe restore -v
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
   mono packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
 fi
