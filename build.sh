b234cc53b83fd56036b711ac583861845b52188a
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-03-12 19:12:32 +0000

    clean up build.cmd/sh scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-dotnet tool restore
-
-#Use paket and not fake for restoring packages
-#c.f. https://github.com/fsharp/FAKE/issues/2181                                                                                                                                                                dotnet paket restore                                                                                                                                                                                               export PAKET_SKIP_RESTORE_TARGETS=true 
-dotnet paket restore
 export PAKET_SKIP_RESTORE_TARGETS=true
 
+dotnet tool restore
+dotnet paket restore
 dotnet fake run build.fsx "$@"
